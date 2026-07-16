d0903b4dc41464056648d56a664dc2a94a105598
Olyad Negero <olyadnegero@gmail.com>
2026-07-16 23:50:09 +0300

    fix: cors issue is fixed to enable the remote frontend to communicate with the remote frontend

--- backend/index.js
+++ backend/index.js
@@ -33,9 +33,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ 
-	origin: process.env.FRONTEND_URL || "http://localhost:5173", 
-	credentials: true 
+app.use(cors({
+	origin: [process.env.FRONTEND_URL, process.env.CLIENT_URL, "http://localhost:5173", "http://localhost:5174"],
+	credentials: true
 }));
 
 app.use(express.json({ limit: '50mb' })); // allows us to parse incoming requests:req.body (increased for image uploads)
